717dcb5658584279a46b23c84e0bc76360289311
rmsstar <robynstar@shaw.ca>
2014-10-16 20:07:48 -0700

    Formatting Changes
    If I screwed something up, shoot me.

--- Lab4 part1/Datapath.vhd
+++ Lab4 part1/Datapath.vhd
@@ -3,12 +3,12 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity Datapath is
-	port (CLOCK			: IN STD_LOGIC;
+	port (clock			: IN STD_LOGIC;
 			reset 		: IN STD_LOGIC;
 			drawing 		: IN STD_LOGIC;
 			erasing		: IN STD_LOGIC;
 			
-			done				: OUT STD_LOGIC;
+			done			: OUT STD_LOGIC;
 			y_val			: OUT std_logic_vector(6 downto 0); 
 			x_val 		: OUT std_logic_vector(7 downto 0);
 			colour		: OUT std_logic_vector(2 downto 0)
@@ -17,57 +17,62 @@ entity Datapath is
 end Datapath;
 
 architecture behavioural of Datapath is
-	signal x      : std_logic_vector(7 downto 0) := (others => '0');
-	signal y      : std_logic_vector(6 downto 0) := (others => '0');
+	signal x  : std_logic_vector(7 downto 0) := (others => '0');
+	signal y  : std_logic_vector(6 downto 0) := (others => '0');
 begin
 
-process(reset, cloCK)
-	variable dx : unsigned(7 downto 0);
-	variable dy : unsigned(6 downto 0);
-	variable var_done : Std_LOGIC;
-begin 
-	if( reset = '1') then 
-		x <= "00000000";
-		y <= "0000000";
-		var_done := '0';
-		done <= '0';
-		
-	elsif (rising_edge(cloCK)) then
-		
-		if(erasing = '1' and var_done = '0') then
-			colour <= "000";
-			if( to_integer( unsigned(x) ) = 160) then
-				y <= std_logic_vector(unsigned(y) + 1);
-				x <= "00000000";
-			else 
-				x <= std_logic_vector(unsigned(x) + 1);
-			end if;
-			if(to_integer(unsigned(y)) = 120) then 
-				y <= "0000000";
-				x <= "00000000";
-				var_done := '1';
-			end if;
-		
-		elsif(drawing = '1') then
-			dx := "00000001";
-			dy := "0000001";
-			colour <= "001";
+	process(reset, clock)
+		variable dx 		: unsigned(7 downto 0);
+		variable dy 		: unsigned(6 downto 0);
+		variable var_done : std_logic;
+	begin 
+		if( reset = '1') then 
+			x <= "00000000";
+			y <= "0000000";
 			var_done := '0';
-			if( to_integer( unsigned(x) ) < 160) then
-				x <= std_logic_vector(unsigned(x) + dx);
-			else
-				var_done := '1';
+			done <= '0';
+			
+		elsif (rising_edge(clock)) then
+			
+			if(erasing = '1' and var_done = '0') then
+				colour <= "000";
+				
+				if( to_integer( unsigned(x) ) = 160) then
+					y <= std_logic_vector(unsigned(y) + 1);
+					x <= "00000000";
+				else 
+					x <= std_logic_vector(unsigned(x) + 1);
+				end if;
+				
+				if(to_integer(unsigned(y)) = 120) then 
+					y <= "0000000";
+					x <= "00000000";
+					var_done := '1';
+				end if;
+			
+			elsif(drawing = '1') then
+				dx := "00000001";
+				dy := "0000001";
+				colour <= "001";
+				var_done := '0';
+				
+				if( to_integer( unsigned(x) ) < 160) then
+					x <= std_logic_vector(unsigned(x) + dx);
+				else
+					var_done := '1';
+				end if;
+				
+				if(to_integer(unsigned(y)) < 120) then
+					y <= std_logic_vector(unsigned(y) + dy);
+				end if;
+				
 			end if;
-			if(to_integer(unsigned(y)) < 120) then
-				y <= std_logic_vector(unsigned(y) + dy);
-			end if;
+			done <= var_done;
 		end if;
-		done <= var_done;
-	end if;
-end process;
+	end process;
 
-y_val <= y;
-x_val <= x;
+	y_val <= y;
+	x_val <= x;
 
  end behavioural;
 	
--- Lab4 part1/FSM.vhd
+++ Lab4 part1/FSM.vhd
@@ -2,7 +2,7 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity FSM is
-	port (CLOCK 				: IN STD_LOGIC;
+	port (clock 				: IN STD_LOGIC;
 			reset 				: IN STD_LOGIC;
 			new_line				: IN STD_LOGIC;
 			finished				: IN STD_LOGIC;
@@ -15,37 +15,37 @@ end FSM;
 architecture behavioural of FSM is 
 
 begin 
-process(reset, cloCK)
-	type state_type is (reset, draw, done);
-	variable state : state_type := reset;
-begin
-	if (reset = '1') then 
-		state := reset;
-		
-	elsif (rising_edge(cloCK)) then
-		if(state = reset) then 
-			erasing <= '1';
-			drawing <= '0';
-			if(finished = '1') then
-				state := done;
-			end if;
+	process(reset, clock)
+		type state_type is (reset, draw, done);
+		variable state : state_type := reset;
+	begin
+		if (reset = '1') then 
+			state := reset;
 			
-		elsif(state = draw) then 
-			erasing <= '0';
-			drawing <= '1';
-			if(finished = '1') then 
-				state := done;
-			end if;
-			
-		else --the done state
-			erasing <= '0';
-			drawing <= '0';
-			if(new_line = '1') then 
-				state := draw;
+		elsif (rising_edge(clock)) then
+			if(state = reset) then 
+				erasing <= '1';
+				drawing <= '0';
+				if(finished = '1') then
+					state := done;
+				end if;
+				
+			elsif(state = draw) then 
+				erasing <= '0';
+				drawing <= '1';
+				if(finished = '1') then 
+					state := done;
+				end if;
+				
+			else --the done state
+				erasing <= '0';
+				drawing <= '0';
+				if(new_line = '1') then 
+					state := draw;
+				end if;
 			end if;
 		end if;
-	end if;
-end process;
+	end process;
 
 end behavioural;
 				
